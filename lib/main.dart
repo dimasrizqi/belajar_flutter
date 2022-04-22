56588ce38627a29c5a240be417383781e691926e
FlutLab.io <>
2022-04-22 03:15:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          'Hello, dimas flutter wakwae',
+          'Hello, dimas flutter wakwae!!!',
         ),
       ),
     );
